b4a9bb23e9154009f437de16e6bdff8075c7f6ad
Chang lue Tsen <tension@ppanel.dev>
2025-05-31 11:30:04 -0400

    fix(proxy): simplify password generation logic in GenerateShadowsocks2022Password

--- pkg/adapter/proxy/tool.go
+++ pkg/adapter/proxy/tool.go
@@ -1,18 +1,15 @@
 package proxy
 
 import (
-	"github.com/perfect-panel/server/pkg/tool"
 	"github.com/perfect-panel/server/pkg/uuidx"
 )
 
 func GenerateShadowsocks2022Password(ss Shadowsocks, password string) (string, string) {
 	// server key
-	var serverKey string
+	serverKey := ss.ServerKey
 	if ss.Method == "2022-blake3-aes-128-gcm" {
-		serverKey = tool.GenerateCipher(ss.ServerKey, 16)
 		password = uuidx.UUIDToBase64(password, 16)
 	} else {
-		serverKey = tool.GenerateCipher(ss.ServerKey, 32)
 		password = uuidx.UUIDToBase64(password, 32)
 	}
 	return serverKey, password
