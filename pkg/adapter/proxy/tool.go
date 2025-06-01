617768a4a1b455864955f8e112fb86406849ed42
Chang lue Tsen <tension@ppanel.dev>
2025-06-01 12:01:02 -0400

    fix(proxy): update server key encoding in GenerateShadowsocks2022Password

--- pkg/adapter/proxy/tool.go
+++ pkg/adapter/proxy/tool.go
@@ -1,16 +1,15 @@
 package proxy
 
 import (
+	"encoding/base64"
 	"github.com/perfect-panel/server/pkg/uuidx"
 )
 
 func GenerateShadowsocks2022Password(ss Shadowsocks, password string) (string, string) {
-	// server key
-	serverKey := ss.ServerKey
 	if ss.Method == "2022-blake3-aes-128-gcm" {
 		password = uuidx.UUIDToBase64(password, 16)
 	} else {
 		password = uuidx.UUIDToBase64(password, 32)
 	}
-	return serverKey, password
+	return base64.StdEncoding.EncodeToString([]byte(ss.ServerKey)), password
 }
